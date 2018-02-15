1f0ce5842524ed0d288b5b40e16a9c82e170c5d1
18jelias <jacquie19_e@yahoo.com>
2018-02-15 16:43:15 -0600

    Landing page with button

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,35 +1,43 @@
 import React, { Component } from 'react';
 import './Landing.css';
 import ReactDOM from 'react-dom';
-import Button from 'material-ui/Button';
 import { Grid, Row, Col } from 'react-material-responsive-grid';
 
+
+var buttonStyle = {
+  padding: '20px 20px 20px 20px',
+  color:'#FFFFFF',
+  'borderRadius': '20px',
+  border: "3px solid #FFFFFF",
+  "fontSize": '20px'
+
+
+}
 class Landing extends Component {
   render() {
     return (
-      <Grid>
-        <Row>
-          <h1>Get Started. Get Unstuck. Get Motivated</h1>
-        </Row>
-        <Row>
-          <h1>The Time is Now</h1>
-        </Row>
-        <Row>
-          JUMPSTART YOUR WRITING
-        </Row>
-        <Row>
-          <Col xs4={2} lg={6}>
-            <Button raised color="primary">
-            Register
-            </Button>
-          </Col>
-          <Col xs4={2} lg={6}>
-            <Button raised color="primary">
-            Start Now
-            </Button>
-          </Col>
-        </Row>
-      </Grid>
+      <div id="container">
+        <Grid>
+          <Row>
+            <h1 id="title">Get Started. Get Unstuck. Get Motivated</h1>
+          </Row>
+          <Row>
+              <img src={require("../assets/JumpstartLogoThick_White.png")} alt="Jumpstart Logo" height="50%" width="50%"/>
+              <h1>The Time is Now</h1>
+          </Row>
+          <Row>
+            <h2 id="slogon">JUMPSTART YOUR WRITING</h2>
+          </Row>
+          <Row>
+            <Col xs4={2} lg={6}>
+
+            </Col>
+            <Col xs4={2} lg={6}>
+
+            </Col>
+          </Row>
+        </Grid>
+      </div>
     );
   }
 }
